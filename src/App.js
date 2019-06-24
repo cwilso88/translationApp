9a53da449e4206f30335e9b264614e1db274c72a
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-06-24 14:11:33 -0400

    Set up onLanguageChange callback function for each language

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ class App extends React.Component {
   state = { language: 'english' };
 
   render() {
+
     return (
       <div className="ui container">
         <div>
           Select A Language: 
-          <i className="flag us"></i>
-          <i className="flag do"></i>
-          <i className="flag br"></i>
+          <i onClick={() => this.onLanguageChange('english')} className="flag us"></i>
+          <i onClick={() => this.onLanguageChange('spanish')} className="flag do"></i>
+          <i onClick={() => this.onLanguageChange('portuguese')} className="flag br"></i>
         </div>
       </div>
     );
