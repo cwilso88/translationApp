7e4d29902fcbf7ed3cf143c05851a992177b3a09
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-06-24 15:20:39 -0400

    Set up ternary experssion for text language output depending on language selected

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import LanguageContex from '../context/LanguageContext';
 
 class Field extends Component {
+    static contextType = LanguageContex;
+
     render() {
+        const text = (this.context === 'English') ? 'Name' : (this.context === 'Spanish') ? 'Nombre' : 'Nome';
+
         return (
             <div className="ui field">
-                <label>Name</label>
+                <label>{text}</label>
                 <input />
             </div>
         )
