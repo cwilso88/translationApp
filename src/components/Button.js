3bce851ff026f04da0550edd9f56a6d7302be220
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-06-25 14:31:12 -0400

    changed value to language in the button comp

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,7 +6,7 @@ class Button extends Component {
         return (
             <button className="ui button primary">
                 <LanguageContext.Consumer>
-                    {(value) => value === 'english' ? 'Submit' : 'Enviar'}
+                    {({ language }) => language === 'english' ? 'Submit' : 'Enviar'}
                 </LanguageContext.Consumer>
             </button>
         )
