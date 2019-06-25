e8f85c75a6bf4e57efdd7c8ee9ee9ee0760e8d7c
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-06-25 14:26:52 -0400

    Changed the this.setState back to just language

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import LanguageContex from '../context/LanguageContext';
+import LanguageContext from '../context/LanguageContext';
 
 class Field extends Component {
-    static contextType = LanguageContex;
+    static contextType = LanguageContext;
 
     render() {
-        const text = (this.context === 'english') ? 'Name' : (this.context === 'spanish') ? 'Nombre' : 'Nome';
+        const text = (this.context.language === 'english') ? 'Name' : (this.context.language === 'spanish') ? 'Nombre' : 'Nome';
 
         return (
             <div className="ui field">
